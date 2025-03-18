7ffc2b80849db0af6f4f802850ec10c25bcce33b
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-03-18 11:30:42 +0700

    perulangan while nilai awal

---
+++ file2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    // Perulangan while
+    cout << "perulangan dengan while" << endl;
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Nilai awal x = " << x << endl;
+    while(x < 7){}
+}
