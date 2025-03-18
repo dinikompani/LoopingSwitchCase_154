5a164a152ceb10dba9b325024c535d0738a4bee4
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-03-18 10:53:37 +0700

    perulangan for pencacah naik

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    //perulangan for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
+    }
+    cout << "Nilai pencacah terakhir = " << cacah;
+    cout << endl;
+    cout << endl;
+}
